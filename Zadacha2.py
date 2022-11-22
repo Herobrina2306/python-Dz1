f28e59704c1ef9df92e2b4c3854b4f85ffc3f824
AleksandraM <musatova.aleksandra@mail.ru>
2022-11-22 14:31:50 +0300

    Задачи решены

--- Zadacha2.py
+++ Zadacha2.py
@@ -1,7 +1,15 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            if not(x or y or z) == not x and not y and not z:
-                print('!({0} or {1} or {2}) = !{0} and !{1} and !{2} '.format(x, y, z))
+coynt = 0
+
+for x in [True, False]:
+    for y in [True, False]:
+        for z in [True, False]:
+            if not(x or y or z) == (not x and not y and not z):
+                coynt = coynt + 1
+                print('{3} !({0} or {1} or {2}) = !{0} and !{1} and !{2} '.format(x, y, z, coynt))
+                if coynt == 8:
+                    print('Утверждение истинно')
+                
+
+
